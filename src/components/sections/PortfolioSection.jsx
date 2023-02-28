0985fbfb386b7dee6ab8742bf69f37900d7fbf8b
Daniel Dimitrov <ddimitrov1108@gmail.com>
2023-02-28 17:26:19 +0200

    portfolio responsive fix on md/lg

--- src/components/sections/PortfolioSection.jsx
+++ src/components/sections/PortfolioSection.jsx
@@ -4,24 +4,21 @@ import portfolioTemplateWebp from "../../assets/project-template.webp";
 
 export default function PortfolioSection() {
   return (
-    <div
-      id="portfolio"
-      className="bg-white overflow-hidden px-4 xs:px-8 py-12"
-    >
+    <div id="portfolio" className="bg-white overflow-hidden px-4 xs:px-8 py-12">
       <Section>
         <SectionHeader>Нашите проекти</SectionHeader>
         <div className="flex flex-col gap-24 items-center justify-start even:justify-end">
           {[1, 2, 3].map((i, index) => (
             <div
               key={index}
-              className="group relative w-full flex flex-col-reverse gap-6 lg:flex-row lg:odd:flex-row-reverse even:lg:text-right"
+              className="group relative w-full flex flex-col-reverse md:flex-row md:odd:flex-row-reverse md:even:text-right"
             >
-              <div className="z-30 lg:absolute lg:top-4 xl:top-12 group-odd:lg:left-4 group-even:lg:right-4">
+              <div className="z-30 md:absolute top-4 xl:top-12 md:group-odd:left-4 md:group-even:right-4">
                 <div className="py-1 text-gold-dark">Featured Project</div>
                 <div className="text-2xl font-semibold text-black-main mb-4">
                   Project Name
                 </div>
-                <div className="text-sm md:text-base min-w-full text-black-light lg:max-w-[540px] xl:max-w-[620px] lg:p-4 lg:bg-secondary">
+                <div className="text-sm lg:text-base min-w-full text-black-light md:max-w-[460px] lg:max-w-[540px] xl:max-w-[620px] md:p-4 md:bg-secondary">
                   Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                   Corrupti suscipit illum omnis veniam, aperiam facilis eveniet
                   earum debitis tenetur voluptas ullam quod tempora ea,
@@ -33,10 +30,13 @@ export default function PortfolioSection() {
                 href="https://www.google.com/"
                 target="_blank"
                 rel="noreferrer"
-                className="relative overflow-hidden rounded-md lg:max-w-[60%] h-auto"
+                className="relative rounded-md md:max-w-[60%] h-auto"
               >
-                <img src={portfolioTemplateWebp} className="w-full" alt="test" />
-                <div className="hidden lg:absolute top-0 right-0 bottom-0 left-0 bg-alternative/40 hover:bg-transparent transition-all duration-300"></div>
+                <img
+                  src={portfolioTemplateWebp}
+                  className="w-full"
+                  alt="test"
+                />
               </a>
             </div>
           ))}
