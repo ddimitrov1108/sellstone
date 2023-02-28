96d4936596fedaecea80f242c65382cba0793d46
Daniel Dimitrov <ddimitrov1108@gmail.com>
2023-02-28 18:51:07 +0200

    .

--- src/components/sections/PortfolioSection.jsx
+++ src/components/sections/PortfolioSection.jsx
@@ -1,6 +1,6 @@
 import Section from "./components/Section";
 import SectionHeader from "./components/SectionHeader";
-import portfolioTemplateWebp from "../../assets/project-template.webp";
+import ProjectCard from "../ui/ProjectCard";
 
 export default function PortfolioSection() {
   return (
@@ -9,36 +9,7 @@ export default function PortfolioSection() {
         <SectionHeader>Нашите проекти</SectionHeader>
         <div className="flex flex-col gap-24 items-center justify-start even:justify-end">
           {[1, 2, 3].map((i, index) => (
-            <div
-              key={index}
-              className="group relative w-full flex flex-col-reverse md:flex-row md:odd:flex-row-reverse md:even:text-right"
-            >
-              <div className="z-30 md:absolute top-4 xl:top-12 md:group-odd:left-4 md:group-even:right-4">
-                <div className="py-1 text-gold-dark">Featured Project</div>
-                <div className="text-2xl font-semibold text-black-main mb-4">
-                  Project Name
-                </div>
-                <div className="text-sm lg:text-base min-w-full text-black-light md:max-w-[460px] lg:max-w-[540px] xl:max-w-[620px] md:p-4 md:bg-secondary">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Corrupti suscipit illum omnis veniam, aperiam facilis eveniet
-                  earum debitis tenetur voluptas ullam quod tempora ea,
-                  distinctio molestiae fugiat repellendus inventore error?
-                </div>
-              </div>
-
-              <a
-                href="https://www.google.com/"
-                target="_blank"
-                rel="noreferrer"
-                className="relative rounded-md md:max-w-[60%] h-auto"
-              >
-                <img
-                  src={portfolioTemplateWebp}
-                  className="w-full"
-                  alt="test"
-                />
-              </a>
-            </div>
+            <ProjectCard    key={index}/>
           ))}
         </div>
       </Section>
---
+++ src/components/ui/ProjectCard.jsx
@@ -0,0 +1,35 @@
+import portfolioTemplateWebp from "../../assets/project-template.webp";
+
+export default function ProjectCard() {
+  return (
+    <div
+    className="group relative w-full flex flex-col-reverse md:flex-row md:odd:flex-row-reverse md:even:text-right"
+  >
+    <div className="z-30 md:absolute md:top-0 lg:top-4 xl:top-12 md:group-odd:left-4 md:group-even:right-4">
+      <div className="py-1 text-gold-dark">Featured Project</div>
+      <div className="text-2xl font-semibold text-black-main mb-4">
+        Project Name
+      </div>
+      <div className="md:shadow-lg text-sm lg:text-base min-w-full text-black-main md:max-w-[420px] lg:max-w-[520px] xl:max-w-[600px] md:p-4 md:bg-secondary">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Corrupti suscipit illum omnis veniam, aperiam facilis eveniet
+        earum debitis tenetur voluptas ullam quod tempora ea,
+        distinctio molestiae fugiat repellendus inventore error?
+      </div>
+    </div>
+
+    <a
+      href="https://www.google.com/"
+      target="_blank"
+      rel="noreferrer"
+      className="relative rounded-md md:max-w-[60%] h-auto"
+    >
+      <img
+        src={portfolioTemplateWebp}
+        className="w-full"
+        alt="test"
+      />
+    </a>
+  </div>
+  )
+}
