fbc2a78b9981eb9a686a8bde0193e09899f42b6b
Daniel Dimitrov <ddimitrov1108@gmail.com>
2023-02-28 21:40:54 +0200

    ui updates

--- src/components/sections/OurTeamSection.jsx
+++ src/components/sections/OurTeamSection.jsx
@@ -1,19 +1,14 @@
 import Section from "./components/Section";
 import SectionHeader from "./components/SectionHeader";
 import { v4 as uuidv4 } from "uuid";
-import happyPersonWebp from "../../assets/happy-person.webp";
+import userPfPng from "../../assets/user-pf.png";
 import { FaLinkedinIn, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const teamArr = [
   {
     id: uuidv4(),
     img: (
-      <img
-        src={happyPersonWebp}
-        width="164px"
-        height="164px"
-        alt="happy-person"
-      />
+      <img src={userPfPng} width="256px" height="256px" alt="happy-person" />
     ),
     name: "Daniel Dimitrov",
     title: "Web Developer",
@@ -34,12 +29,7 @@ const teamArr = [
   {
     id: uuidv4(),
     img: (
-      <img
-        src={happyPersonWebp}
-        width="164px"
-        height="164px"
-        alt="happy-person"
-      />
+      <img src={userPfPng} width="256px" height="256px" alt="happy-person" />
     ),
     name: "Daniel Dimitrov",
     title: "Web Developer",
@@ -60,12 +50,7 @@ const teamArr = [
   {
     id: uuidv4(),
     img: (
-      <img
-        src={happyPersonWebp}
-        width="164px"
-        height="164px"
-        alt="happy-person"
-      />
+      <img src={userPfPng} width="256px" height="256px" alt="happy-person" />
     ),
     name: "Daniel Dimitrov",
     title: "Web Developer",
@@ -94,23 +79,23 @@ export default function OurTeamSection() {
       <Section>
         <SectionHeader>Нашият екип</SectionHeader>
 
-        <div className="flex flex-col md:flex-row gap-10 gap-y-4 items-center justify-center">
+        <div className="flex flex-col md:flex-row gap-14 gap-y-4 items-center justify-center">
           {teamArr.map((employee) => (
             <div
               key={employee.id}
-              className="hidden-section employee-card grid gap-2 rounded-md p-4"
+              className="bg-white md:shadow-lg overflow-hidden hidden-section employee-card grid gap-6 rounded-md p-4"
             >
-              <div className="relative rounded-full overflow-hidden w-fit mx-auto p-4 bg-alternative border-2 border-gold-main">
+              <div className="relative overflow-hidden w-fit mx-auto p-4 pb-0 bg-alternative rounded-md">
                 {employee.img}
                 <div className="group absolute top-0 right-0 bottom-0 left-0 hover:bg-alternative/60 transition-all duration-300">
-                  <div className="w-full h-full flex items-center justify-center gap-4 text-3xl transform transition-all duration-500 translate-y-full group-hover:translate-y-0">
+                  <div className="w-full h-full flex items-center justify-center gap-4 text-3xl transform transition-all duration-500 translate-y-40 group-hover:translate-y-0">
                     {employee.socials.map((social) => (
                       <a
                         key={social.id}
                         href={social.url}
                         target="_blank"
                         rel="noreferrer"
-                        className="transition-all duration-300 text-black-main"
+                        className="transition-all duration-300"
                       >
                         {social.icon}
                       </a>
@@ -118,12 +103,14 @@ export default function OurTeamSection() {
                   </div>
                 </div>
               </div>
-              <span className="w-fit mx-auto text-center text-lg truncate ...">
-                {employee.name}
-              </span>
-              <span className="text-black-main/40 w-fit mx-auto text-center font-semibold truncate ...">
-                {employee.title}
-              </span>
+              <div className="grid items-center gap-2">
+                <span className="w-fit mx-auto text-center text-lg font-semibold truncate ...">
+                  {employee.name}
+                </span>
+                <span className="text-black-main/80 w-fit mx-auto text-center truncate ...">
+                  {employee.title}
+                </span>
+              </div>
             </div>
           ))}
         </div>
--- src/components/ui/ServiceCard.jsx
+++ src/components/ui/ServiceCard.jsx
@@ -1,13 +1,13 @@
 export default function ServiceCard({ service = null }) {
   return (
-    <div className="hidden-section service-card select-none transition-all duration-300 gap-6 grid items-center justify-center sm:p-8 bg-white rounded-md w-full md:max-w-[330px] lg:max-w-[360px]">
+    <div className="md:shadow-lg hidden-section service-card select-none transition-all duration-300 gap-6 grid items-center justify-center sm:p-8 bg-white rounded-md w-full md:max-w-[330px] lg:max-w-[360px]">
       <div className="mx-auto rounded-full bg-alternative p-4">
         {service?.icon}
       </div>
       <span className="w-fit mx-auto text-center font-semibold text-lg border-b-2 border-gold-main px-4 py-2 truncate ...">
         {service?.name}
       </span>
-      <p className="text-sm md:text-base text-black-light">
+      <p className="text-sm text-black-light">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita ullam
         nobis omnis reprehenderit aspernatur asperiores fuga delectus voluptate.
         Odit nesciunt expedita, recusandae fugiat a temporibus? Repellat impedit
