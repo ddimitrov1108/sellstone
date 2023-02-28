a8c32f1b176272e2f5c6c7b04ac62ce6972ef013
Daniel Dimitrov <ddimitrov1108@gmail.com>
2023-02-28 13:29:05 +0200

    seo updates

--- src/components/ui/formik/Label.jsx
+++ src/components/ui/formik/Label.jsx
@@ -1,9 +1,9 @@
 import clsx from "clsx";
 
-export default function Label({ text, className, ...restProps }) {
+export default function Label({ children, className, ...restProps }) {
   return (
     <div className={clsx("min-w-fit pb-1.5", className)} {...restProps}>
-      <span className="min-w-fit text-sm">{text}</span>
+      <label className="min-w-fit text-sm text-black-light">{children}</label>
     </div>
   );
 }
