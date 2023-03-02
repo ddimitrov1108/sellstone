6658e8fb421f5e96e54ab2086ed8637bf8bd0718
Daniel Dimitrov <ddimitrov1108@gmail.com>
2023-03-02 21:05:53 +0200

    .

--- src/components/ui/ProjectCard.jsx
+++ src/components/ui/ProjectCard.jsx
@@ -9,7 +9,7 @@ export default function ProjectCard() {
         <div className="text-2xl font-semibold text-black-main mb-4">
           Project Name
         </div>
-        <div className="opacity-0 project-card-info sm:shadow-lg text-sm lg:text-base min-w-full text-black-main md:max-w-[420px] lg:max-w-[520px] xl:max-w-[600px] md:p-4 md:bg-secondary">
+        <div className="opacity-0 project-card-info rounded-md sm:shadow-lg text-sm lg:text-base min-w-full text-black-main md:max-w-[420px] lg:max-w-[520px] xl:max-w-[600px] md:p-4 md:bg-secondary">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
           suscipit illum omnis veniam, aperiam facilis eveniet earum debitis
           tenetur voluptas ullam quod tempora ea, distinctio molestiae fugiat
