73b23ae6597678786938ea0b59691548973c0d7f
Daniel Dimitrov <ddimitrov1108@gmail.com>
2023-03-07 23:04:14 +0200

    .

--- src/components/sections/PartnersSection.jsx
+++ src/components/sections/PartnersSection.jsx
@@ -6,13 +6,15 @@ import redbullPng from "../../assets/redbull.png";
 import kauflandPng from "../../assets/kaufland.png";
 import obbPng from "../../assets/obb.png";
 import xellaPng from "../../assets/xella.png";
+import { v4 as uuidv4 } from "uuid";
+
 const partnersImgs = [
-  devinPng,
-  lukoilPng,
-  redbullPng,
-  kauflandPng,
-  obbPng,
-  xellaPng,
+  { id: uuidv4(), href: "http://www.devin-bg.com/en/", img: devinPng },
+  { id: uuidv4(), href: "https://lukoil.bg/bg/", img: lukoilPng },
+  { id: uuidv4(), href: "https://www.redbull.com/bg-bg", img: redbullPng },
+  { id: uuidv4(), href: "https://www.kaufland.bg/", img: kauflandPng },
+  { id: uuidv4(), href: "https://www.ubb.bg/", img: obbPng },
+  { id: uuidv4(), href: "https://www.xella.bg/", img: xellaPng },
 ];
 
 export default function PartnersSection() {
@@ -31,7 +33,7 @@ export default function PartnersSection() {
             </div>
 
             <h1 className="text-primaryHeaderText section-header md:text-left text-3xl sm:text-5xl font-semibold">
-              Работим с най-добрите партньори
+              Партньорите, с които работим
             </h1>
 
             <p className="text-[#B7BDC6]">
@@ -42,11 +44,11 @@ export default function PartnersSection() {
             </p>
           </div>
 
-          <div className="w-full flex flex-wrap items-center text-center justify-center gap-y-8 gap-x-4 md:gap-12">
-            {partnersImgs.map((img, index) => (
+          <div className="w-full flex flex-wrap items-center text-center justify-center gap-y-8 gap-x-8 md:gap-12">
+            {partnersImgs.map(({id, href, img}) => (
               <a
-                key={index}
-                href="https://www.google.com/"
+                key={id}
+                href={href}
                 target="_blank"
                 rel="noreferrer"
                 className="flex items-center mx-auto w-[20%] h-fit"
@@ -56,11 +58,7 @@ export default function PartnersSection() {
                   width="256"
                   height="auto"
                   alt="partner_logo.webp"
-                  className=""
-                  style={{
-                    filter:
-                      "invert(67%) sepia(4%) saturate(1022%) hue-rotate(176deg) brightness(82%) contrast(88%)",
-                  }}
+                  className="img-filter-hover"
                 />
               </a>
             ))}
