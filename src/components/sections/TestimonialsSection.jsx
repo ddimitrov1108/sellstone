74d488d8d153e280e5e6b4c814285ba87e2d4d10
Daniel Dimitrov <ddimitrov1108@gmail.com>
2023-03-09 17:04:53 +0200

    .

--- src/components/sections/TestimonialsSection.jsx
+++ src/components/sections/TestimonialsSection.jsx
@@ -3,6 +3,59 @@ import Section from "./components/Section";
 import SectionHeader from "./components/SectionHeader";
 import userPfWebp from "../../assets/user-pf.webp";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import { v4 as uuidv4 } from "uuid";
+
+import testimonial1Webp from "../../assets/testimonial-1.webp";
+import testimonial2Webp from "../../assets/testimonial-2.webp";
+import testimonial3Webp from "../../assets/testimonial-3.webp";
+import testimonial4Webp from "../../assets/testimonial-4.webp";
+import testimonial5Webp from "../../assets/testimonial-5.webp";
+import testimonial6Webp from "../../assets/testimonial-6.webp";
+
+const testimonialsArr = [
+  {
+    id: uuidv4(),
+    name: "Яна Вълчева",
+    img: testimonial1Webp,
+    description:
+      "Трябва да кажа, че бях поразена от резултатите, които постигнахме, работейки със Sellstone. Наистина имах чувството, че имат моите най-добри интереси в сърцето си.",
+  },
+  {
+    id: uuidv4(),
+    name: "Стоян Михайлов",
+    img: testimonial2Webp,
+    description:
+      "Като собственик на малък бизнес не бях сигурен как ще протече едно сътрудничество с външна агенция, но за мое щастие си заслужаваше.Те винаги бяха на разположение, за да отговорят на въпросите ми и да предложат насоки.",
+  },
+  {
+    id: uuidv4(),
+    name: "Ясен Тодоров",
+    img: testimonial3Webp,
+    description:
+      "Колебах се дали да работя с маркетингова агенция, но този екип ме накара да се чувствам удобно от самото начало.",
+  },
+  {
+    id: uuidv4(),
+    name: "Дарин Ангелов",
+    img: testimonial4Webp,
+    description:
+      "Екипът им е не само знаещ и квалифициран, но и приятелски настроен и персонален. Отделиха време да опознаят нашия екип и нашия бизнес, което направи процеса на сътрудничество безпроблемен.",
+  },
+  {
+    id: uuidv4(),
+    name: "Александър Димитров",
+    img: testimonial5Webp,
+    description:
+      "От момента, в който започнахме да работим с тази маркетингова агенция, знаех, че сме в добри ръце. Екипът им е страстен за това, което правят и това си личи в работата им.",
+  },
+  {
+    id: uuidv4(),
+    name: "Велизара Димова",
+    img: testimonial6Webp,
+    description:
+      "Като нов собственик на бизнес не бях сигурна откъде да започна. Техният екип отдели време да изслуша моите идеи и ми помогна да изградя силно онлайн присъствие.",
+  },
+];
 
 const params = {
   centeredSlides: false,
@@ -50,31 +103,25 @@ export default function TestimonialsSection() {
             autoplay-delay="5000"
             autoplay-disable-on-interaction="false"
           >
-            {[...Array(9).keys()].map((key) => (
-              <swiper-slide key={key}>
-                <div className="w-full bg-black-dark rounded-lg mt-14">
-                  <LazyLoadImage
-                    src={userPfWebp}
-                    width="128px"
-                    height="128px"
-                    alt="user_pf.webp"
-                    className="shadow-md mx-auto bg-black-main border-2 border-gold-light rounded-full -mt-24"
-                  />
+            {testimonialsArr.map(({ id, name, img, description }) => (
+              <swiper-slide key={id}>
+                <div className="w-full bg-black-dark rounded-lg mt-14 ">
+                  <div className="mx-auto border-2 w-32 h-32 flex overflow-hidden border-gold-light rounded-full -mt-24">
+                    <LazyLoadImage
+                      src={img}
+                      width="128"
+                      height="128"
+                      alt="user_pf.webp"
+                      className="shadow-md mx-auto bg-black-main object-cover"
+                    />
+                  </div>
 
-                  <div className="mt-4 py-4 flex items-center gap-2">
-                    <div className="z-30 text-primaryHeaderText p-4 text-sm">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Eveniet, numquam? Aut sapiente accusantium quas doloribus
-                      earum maxime, id distinctio! Corporis facere sint
-                      voluptatum nostrum nesciunt ea, debitis hic nihil
-                      asperiores repellat facilis delectus veniam quisquam nam
-                      sapiente unde? Exercitationem, ducimus!
-                    </div>
+                  <div className="mt-4 min-h-[150px] grow z-30 text-primaryHeaderText p-4 text-sm">
+                    {description}
                   </div>
 
                   <div className="text-center text-primaryHeaderText">
-                    <span className="font-semibold">Daniel Dimitrov,</span>{" "}
-                    <span className="text-gold-main"> Bulgaria</span>
+                    <span className="font-semibold text-gold-main">{name}</span>
                   </div>
                 </div>
               </swiper-slide>
